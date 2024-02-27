7461772e8bd24babc1a82cea53802a84de63145e
Zuzanna Radosz <107064508+ZuzRad@users.noreply.github.com>
2024-02-27 09:49:22 +0100

    1 a) Find a bug in the code logic and fix it

--- Assets/Scripts/Items/InventoryController.cs
+++ Assets/Scripts/Items/InventoryController.cs
@@ -13,16 +13,16 @@
 
 		public void SellAllItemsUpToValue(int maxValue)
 		{
-			for (var i = 0; i < items.Count; i++)
-			{
-				var itemValue = items[i].Value;
-				if (itemValue > maxValue)
-					continue;
-				
-				money += itemValue;
-				items.RemoveAt(i);
-			}
-		}
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                var itemValue = items[i].Value;
+                if (itemValue <= maxValue)
+                {
+                    money += itemValue;
+                    items.RemoveAt(i);
+                }
+            }
+        }
 
 		public void AddItem(Item item)
 		{
